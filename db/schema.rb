2ab14ac52c255a90f5b43ec5f06386bcee5a22f1
nijimajohn <hirokikotera0211@gmail.com>
2021-08-12 10:03:38 +0000

    [フォローフォロワー機能]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_083040) do
+ActiveRecord::Schema.define(version: 2021_08_12_091931) do
 
   create_table "dog_comments", force: :cascade do |t|
     t.text "comment"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2021_08_12_083040) do
     t.text "comment"
     t.integer "user_id"
     t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
